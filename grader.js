c626b963a12a73150021d442aa064d73fdb3d0a9
Sanjay Cherian <scherian@charter.net>
2013-07-10 22:55:02 +0000

    step 3

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "file://index.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +57,27 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var buildfn = function(checksfile) {
+    var handleUrl = function(data, result) {
+	$ = cheerio.load(data);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for (var ii in checks) {
+	    var present = $(checks[ii]).length > 0;
+	    out[checks[ii]] = present;
+	}
+        var outJson = JSON.stringify(out, null, 4);
+        console.log(outJson);
+    };
+    return handleUrl;
+};
+
+var checkUrl = function(targetUrl, checksfile) {
+    console.log("checkUrl called with %s, %s", targetUrl, checksfile);
+    var handleUrl = buildfn(checksfile);
+    rest.get(targetUrl).on('complete', handleUrl);
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +88,16 @@ if (require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --targetUrl <url>', 'URL to check')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.targetUrl) {
+	checkUrl(program.targetUrl, program.checks);
+    } else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkUrl = checkUrl;
 }
